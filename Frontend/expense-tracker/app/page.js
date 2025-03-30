20140edd3b33694af9e54ef4bc90d64721ef9367
Sushil Kumar Yadav <sushilyadukul@gmail.com>
2025-03-30 13:08:39 +0530

    add logo

--- Frontend/expense-tracker/app/page.js
+++ Frontend/expense-tracker/app/page.js
@@ -1,14 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { Button } from "@/components/ui/button";
+import Header from "./_components/Header";
 
 export default function Home() {
   return (
     <div>
-      <h2>Hello here!!!</h2>
-      <Button
-      variant = "outline"
-      >Button</Button>
+      <Header/>
     </div>
   );
 }
