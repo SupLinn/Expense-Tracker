7768548115ccb281e4f610ab7a07844725179f4c
Sushil Kumar Yadav <sushilyadukul@gmail.com>
2025-04-05 15:29:43 +0530

    budget and expense list on dashboard added

--- Frontend/expense-tracker/app/(routes)/dashboard/page.jsx
+++ Frontend/expense-tracker/app/(routes)/dashboard/page.jsx
@@ -6,9 +6,12 @@ import { db } from '@/utils/dbConfig'
 import { Budgets, Expenses } from '@/utils/schema'
 import { desc, eq, getTableColumns, sql } from 'drizzle-orm'
 import BarChartDashboard from './_components/BarChartDashboard'
+import BudgetItem from './budgets/_components/BudgetItem'
+import ExpenseListTable from './expenses/_components/ExpenseListTable'
 
 function Dashboard() {
   const [budgetList, setBudgetList] = useState([])
+  const [expensesList, setExpensesList] = useState([])
   
     const {user} =useUser()
     useEffect(()=>{
@@ -28,20 +31,45 @@ function Dashboard() {
       .orderBy(desc(Budgets.id))
       
       setBudgetList(result)
+      getAllExpenses()
       
     }
+
+    const getAllExpenses = async () => {
+      const result = await db.select({
+        id: Expenses.id,
+        name: Expenses.name,
+        amount: Expenses.amount,
+        createdAt: Expenses.createdAt
+      }).from(Budgets)
+      .rightJoin(Expenses, eq(Budgets.id, Expenses.budgetId))
+      .where(eq(Budgets.createBy,user?.primaryEmailAddress.emailAddress))
+      .orderBy(desc(Expenses.id))
+      setExpensesList(result)
+
+      console.log(result);
+      
+    }
+
+
   return (
     <div className='p-8'>
       <h2 className='font-bold text-3xl'>Hi, {user?.fullName} </h2>
       <p className='text-gray-500'>Here's what happening with your money, Let's manage your expense.</p>
       <CardInfo budgetList={budgetList}/>
-      <div className='grid grid-cols-1 md:grid-cols-3 mt-6'>
+      <div className='grid grid-cols-1 md:grid-cols-3 mt-6 gap-5'>
         <div className='md:col-span-2'>
           <BarChartDashboard
           budgetList={budgetList}/>
+          <ExpenseListTable expensesList={expensesList}
+          refreshData={()=>getBudgetList()} />
+
         </div>
-        <div>
-          Other Content
+        <div className='grid gap-5'>
+          <h2 className='font-bold text-lg'>Latest Budgets</h2>
+          {budgetList.map((budget, index) => (
+            <BudgetItem budget={budget} key={index} />
+          ))}
         </div>
       </div>
     </div>
