c296309494e6bc5b73da6f60cba8bea6a3cee63a
Sushil Kumar Yadav <sushilyadukul@gmail.com>
2025-04-05 14:52:38 +0530

    added bar chart

--- Frontend/expense-tracker/app/(routes)/dashboard/page.jsx
+++ Frontend/expense-tracker/app/(routes)/dashboard/page.jsx
@@ -5,6 +5,7 @@ import CardInfo from './_components/CardInfo'
 import { db } from '@/utils/dbConfig'
 import { Budgets, Expenses } from '@/utils/schema'
 import { desc, eq, getTableColumns, sql } from 'drizzle-orm'
+import BarChartDashboard from './_components/BarChartDashboard'
 
 function Dashboard() {
   const [budgetList, setBudgetList] = useState([])
@@ -34,6 +35,15 @@ function Dashboard() {
       <h2 className='font-bold text-3xl'>Hi, {user?.fullName} </h2>
       <p className='text-gray-500'>Here's what happening with your money, Let's manage your expense.</p>
       <CardInfo budgetList={budgetList}/>
+      <div className='grid grid-cols-1 md:grid-cols-3 mt-6'>
+        <div className='md:col-span-2'>
+          <BarChartDashboard
+          budgetList={budgetList}/>
+        </div>
+        <div>
+          Other Content
+        </div>
+      </div>
     </div>
   )
 }
